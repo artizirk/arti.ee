4759b73b5812e5439f4d1bda12f3dd5d35755a20
Arti Zirk <arti.zirk@gmail.com>
2014-06-13 19:51:50 +0300

    Got debug toolbar working

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 from flask.ext.script import Manager, Server
+from flask_debugtoolbar import DebugToolbarExtension
 
 from blog import app
 
 app.config["DEBUG"] = True
+toolbar = DebugToolbarExtension(app)
 manager = Manager(app)
 manager.add_command("runserver", Server(host="0.0.0.0"))
 
